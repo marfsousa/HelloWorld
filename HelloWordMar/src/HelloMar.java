85443614f9ad2bcfd240ac451124e9d0334f903c
marfs <marfs@LAPTOP-NSDSU5R2>
2017-02-26 22:06:12 -0300

    Adicionado mais uma linha de mensagem ao método main.

--- HelloWordMar/src/HelloMar.java
+++ HelloWordMar/src/HelloMar.java
@@ -4,6 +4,7 @@ public class HelloMar {
 	public static void main(String args[]) {
 		
 		System.out.println("Olá Marcelo!");
+		System.out.println("Isso deve ser o suficiente para o começo.");
 
 	}
 
